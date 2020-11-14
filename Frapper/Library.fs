79dc2758cfc81e9de934996cab6d78390310e60b
Pawel Zelmanski <pzelmanski1@gmail.com>
2020-11-14 08:37:13 +0100

    init

---
+++ Frapper/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Frapper
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ build.fsx
@@ -0,0 +1,32 @@
+﻿#r "paket:
+nuget Fake.Core.Target //"
+#load ".fake/build.fsx/intellisense.fsx"
+
+
+open Fake.Core
+
+// *** Define Targets ***
+Target.create "Default" (fun _ -> 
+    Trace.log "Default fake"
+)
+
+Target.create "Clean" (fun _ -> 
+    Trace.log "--- CLEANING ---"
+)
+
+Target.create "Build" (fun _ ->
+    Trace.log "--- BUILDING ---"
+)
+
+Target.create "Deploy" (fun _ ->
+    Trace.log "--- DEPLOYING ---"
+)
+
+open Fake.Core.TargetOperators
+
+// *** Define Dependencies ***
+"Clean"
+  ==> "Default"
+
+// *** Start Build ***
+Target.runOrDefault "Default"
